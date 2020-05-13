510cb67b8065ae358c9b6fc1709b86a849c75ce1
Ivan Lisin <ivanlisin.tech@gmail.com>
2020-05-13 19:49:14 +0700

    add test for plain format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,9 +2,12 @@ import genDiff from '../src';
 
 const dir = '__fixtures__';
 const pathToDir = `${__dirname}/../${dir}`;
-const defaultFormat = 'stylish';
-const notSupportedFormat = 'star';
-const diff = `{
+
+const format1 = 'stylish';
+const format2 = 'plain';
+const notSupportedFormat = 'some format';
+
+const diff1 = `{
     common: {
         setting1: Value 1
       - setting2: 200
@@ -38,15 +41,25 @@ const diff = `{
         fee: 100500
     }
 }`;
+const diff2 = `Property common.setting2 was deleted
+Property common.setting3 was changed from true to [complex value]
+Property common.setting6.ops was added with value: vops
+Property common.follow was added with value: false
+Property common.setting4 was added with value: blah blah
+Property common.setting5 was added with value: [complex value]
+Property group1.baz was changed from bas to undefined
+Property group1.nest was changed from [complex value] to str
+Property group2 was deleted
+Property group3 was added with value: [complex value]`;
 
-test('gendiff for recursive files', () => {
-  expect(genDiff(`${pathToDir}/before.json`, `${pathToDir}/after.json`, defaultFormat)).toEqual(diff);
-  expect(genDiff(`${pathToDir}/before.yaml`, `${pathToDir}/after.yaml`, defaultFormat)).toEqual(diff);
+test('gendiff', () => {
+  expect(genDiff(`${pathToDir}/before.json`, `${pathToDir}/after.json`, format1)).toEqual(diff1);
+  expect(genDiff(`${pathToDir}/before.yaml`, `${pathToDir}/after.yaml`, format1)).toEqual(diff1);
 
-  expect(genDiff(`${dir}/before.yml`, `${dir}/after.yml`, defaultFormat)).toEqual(diff);
-  expect(genDiff(`${dir}/before.ini`, `${dir}/after.ini`, defaultFormat)).toEqual(diff);
+  expect(genDiff(`${dir}/before.yml`, `${dir}/after.yml`, format2)).toEqual(diff2);
+  expect(genDiff(`${dir}/before.ini`, `${dir}/after.ini`, format2)).toEqual(diff2);
 
-  expect(() => genDiff(`${pathToDir}/not-exist-1.json`, `${pathToDir}/not-exist-2.json`, defaultFormat)).toThrow();
-  expect(() => genDiff(`${pathToDir}/before.txt`, `${pathToDir}/after.txt`, defaultFormat)).toThrowError('.txt not supported');
+  expect(() => genDiff(`${pathToDir}/not-exist-1.json`, `${pathToDir}/not-exist-2.json`, format1)).toThrow();
+  expect(() => genDiff(`${pathToDir}/before.txt`, `${pathToDir}/after.txt`, format1)).toThrowError('.txt not supported');
   expect(() => genDiff(`${pathToDir}/before.json`, `${pathToDir}/after.json`, notSupportedFormat)).toThrowError(`${notSupportedFormat} not supported`);
 });
