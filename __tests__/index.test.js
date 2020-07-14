f869dff153f0514b84bebb2fe8e114ed03323ab3
Ivan Lisin <ivanlisin.tech@gmail.com>
2020-07-14 17:16:14 +0700

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,55 +1,17 @@
 import path from 'path';
 import fs from 'fs';
 import genDiff from '../src';
-import diff from '../__fixtures__/diff';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let outputStylish;
-let outputPlain;
-let outputJson;
-
-beforeAll(() => {
-  outputStylish = readFile('stylish.txt');
-  outputPlain = readFile('plain.txt');
-  outputJson = JSON.stringify(diff);
-});
-
-test('gendiff', () => {
-  expect(genDiff(
-    getFixturePath('before.json'),
-    getFixturePath('after.json'),
-    'stylish',
-  )).toEqual(outputStylish);
-
-  expect(genDiff(
-    getFixturePath('before.yml'),
-    getFixturePath('after.yml'),
-    'plain',
-  )).toEqual(outputPlain);
-
-  expect(genDiff(
-    getFixturePath('before.ini'),
-    getFixturePath('after.ini'),
-    'json',
-  )).toEqual(outputJson);
-
-  expect(() => genDiff(
-    getFixturePath('not-exist-1.json'),
-    getFixturePath('not-exist-2.json'),
-    'stylish',
-  )).toThrow();
-
-  expect(() => genDiff(
-    getFixturePath('before.txt'),
-    getFixturePath('after.txt'),
-    'stylish',
-  )).toThrowError('extension txt not supported');
-
-  expect(() => genDiff(
-    getFixturePath('before.json'),
-    getFixturePath('after.json'),
-    'notSupportedFormat',
-  )).toThrowError('format notSupportedFormat not supported');
+test.each([
+  ['before.json', 'after.json', 'stylish', 'diff.stylish'],
+  ['before.yml', 'after.yml', 'plain', 'diff.plain'],
+  ['before.ini', 'after.ini', 'json', 'diff.json'],
+])('gendiff with %s %s', (filename1, filename2, format, filenameDiff) => {
+  const path1 = getFixturePath(filename1);
+  const path2 = getFixturePath(filename2);
+  const expected = readFile(filenameDiff);
+  expect(genDiff(path1, path2, format)).toBe(expected);
 });
